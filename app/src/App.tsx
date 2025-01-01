2b73b01b6812b1aa0350a323c26fbc000dee7f65
llthompson <lisalacythompson+github@gmail.com>
2024-12-31 18:20:42 -0600

    inital css

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import cinnamonRoll from './assets/cinnamonroll.png';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,24 +10,19 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        {/* <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </a> */}
       </div>
-      <h1>Vite + React</h1>
+      <h1>Synonym Roll</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <img src={cinnamonRoll} alt="Cinnamon Roll" />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className="game-owners">
+        A game created and owned by Lisa Thompson and Jesse Thompson
       </p>
     </>
   )
